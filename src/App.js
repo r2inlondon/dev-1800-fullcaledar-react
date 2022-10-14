5675cef32acf111cb3f7942c70e82ccc7416380f
Arturo Quiroz <r2inlondon@gmail.com>
2022-10-14 12:43:56 +0100

    feat : basic calendar render is set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
-function App() {
-  return <h1>Creating FullCalendar</h1>;
+import React from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+// import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
+
+export default class DemoApp extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+        headerToolbar={{
+          left: "dayGridMonth,timeGridWeek,timeGridDay",
+          center: "title",
+          right: "prevYear,prev,next,nextYear",
+        }}
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        initialView="timeGridWeek"
+      />
+    );
+  }
 }
-export default App;
